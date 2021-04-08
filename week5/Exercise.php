0ba02e8efd2e767584ab8e5a998a6468d6df103d
Hang Tran <hang40338@gmail.com>
2021-04-08 22:38:21 +0700

    finish practice week 5

---
+++ week5/Exercise.php
@@ -0,0 +1,19 @@
+<?php
+class Page
+{
+    private $page, $title, $copyright;
+    private $year;
+
+    function addHeader()
+    {
+    }
+    private function addFooter()
+    {
+    }
+    function get()
+    {
+    }
+    function addContent($content)
+    {
+    }
+}
