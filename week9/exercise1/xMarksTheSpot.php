e9d39eb863b21416f56e70f156ea6cf7060906fc
DESKTOP-9NGASCB\DELL <honghue2911999@gmail.com>
2021-05-17 22:13:09 +0700

    add ex3

---
+++ week9/exercise1/xMarksTheSpot.php
@@ -0,0 +1,7 @@
+<?php // set name of XML file
+ $file = "xMarksTheSpot.xml"; 
+// load file 
+$xml = simplexml_load_file($file) or die ("Unable to load XML file!");
+// get all the <desc> elements and print 
+foreach ($xml->xpath('//item[quantity > 1]/desc') as $desc) 
+    { echo "$desc\n"; } ?>
